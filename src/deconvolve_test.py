6d52f330407b46bc04732035835263c6b2684f50
ryancoffee <coffeer76@gmail.com>
2019-06-20 21:53:59 -0700

    incremental deconv tests

--- src/deconvolve_test.py
+++ src/deconvolve_test.py
@@ -20,8 +20,14 @@ def weiner(X,N):
 
 def main():
     nhits = 10
+    ofscale = 20
+    ufscale = 75
     if 1<len(sys.argv):
         nhits = int(sys.argv[1])
+        if 2< len(sys.argv):
+            ofscale = int(sys.argv[2])
+            if 3< len(sys.argv):
+                ufscale = int(sys.argv[3])
     x = np.arange(667,dtype=int)
     nscale=3e-1
     n = np.random.normal(0,nscale,len(x))
@@ -29,20 +35,27 @@ def main():
     F = gauss(f,0,1./5.)
     y = np.zeros(x.shape,dtype=float)
     inds = np.random.choice(x,nhits)
-    y[inds] = [np.random.sample(len(inds))]
+    y[inds] = [np.abs(np.random.normal(1,.5,len(inds)))]
     s = sig(x,20,3,1)
     S = np.fft.fft(s)
     DS = 1j*f*S
     SDS = S+DS
     W = weiner(S,nscale)
+    df = f[1]-f[0]
+    OF = gauss(f,0,ofscale*df)
     Y = np.fft.fft(y)
     yg = np.fft.ifft(Y*(SDS))+nscale*n
     YG = np.fft.fft(yg.real)
     yd = np.fft.ifft(YG/(SDS))
     yf = np.fft.ifft(YG*W)
+    yof = np.fft.ifft(YG*OF)
     yc = np.fft.ifft(YG*(-S))
+    ycof = np.fft.ifft(YG*(-S)*OF)
+    yfinal = yc*np.abs(ycof)
 
-    np.savetxt('data_fs/processed/deconvolve.out',np.column_stack((x,y,s,yg.real,yd.real,yf.real,yc.real)),fmt='%.6f')
+    weightedinds = np.fft.ifft(np.fft.fft(x*yc)*gauss(f,0,df*ufscale))/np.fft.ifft(np.fft.fft(yc)*gauss(f,0,df*ufscale))
+
+    np.savetxt('data_fs/processed/deconvolve.out',np.column_stack((x,y,s,yg.real,yd.real,yf.real,yof.real,yc.real,ycof.real,yfinal.real,weightedinds.real)),fmt='%.6f')
     np.savetxt('data_fs/processed/deconvolve.fft',np.column_stack((f,np.abs(Y),np.abs(SDS),np.abs(YG),np.abs(YG*W))),fmt='%.6f')
     return
 
