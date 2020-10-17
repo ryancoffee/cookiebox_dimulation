2e0e749f2b0c7e11a639c5a0dfce54ea8bcd8348
Ryan Coffee <coffeer76@gmail.com>
2020-10-16 21:08:41 -0700

    incremental

--- src/generate_sinogram.py
+++ src/generate_sinogram.py
@@ -7,41 +7,46 @@ from matplotlib import cm
 
 
 def main():
-    if len(sys.argv)<2:
-        print('syntax:\t%s <outputfilename> <streakamp optional>'%(sys.argv[0]))
+    if len(sys.argv)<3:
+        print('syntax:\t%s <outputfilehead> <nimages> <streakamp optional>'%(sys.argv[0]))
         return
     streakamp = 20.
+    nimages = 10
     if len(sys.argv)>2:
-        streakamp = float(sys.argv[2])
+        nimages = int(sys.argv[2])
+    if len(sys.argv)>3:
+        streakamp = float(sys.argv[3])
 
-    outname = sys.argv[1]
-    nangles = 128 
-    nenergies = 128
+    outhead = sys.argv[1]
+    nangles = 256 
+    nenergies = 256 
     emin = 0
     emax = 100
-    npulses = int(np.random.uniform(3,8))
-
 
     etotalwidth = 10.
     ecentral = 50.
-    
-    ecenters = np.random.normal(ecentral,etotalwidth,(npulses,))
-    ewidths = np.random.gamma(5.,.25,(npulses,))+1.25
-    ephases= np.random.uniform(0.,2.*np.pi,(npulses,))
-
     angles = np.linspace(0,np.pi*2.,nangles+1)
     energies = np.linspace(emin,emax,nenergies+1)
     scale = 100
-    hist_ens = np.zeros((nenergies,nangles),dtype=int)
-    for i in range(npulses):
-        c0 = 1.
-        c2 = np.random.uniform(-1,1)
-        c4 = np.random.uniform(-(c0+c2),c0+c2)
-        poldist = np.polynomial.legendre.Legendre([c0,0,c2,0,c4])(np.cos(angles[:-1]))
-        for a in range(nangles):
-            ncounts = int(poldist[a] * scale)
-            streak = streakamp*np.cos(angles[a]-ephases[i])
-            hist_ens[:,a] += np.histogram(np.random.normal(ecenters[i]+streak,ewidths[i],(ncounts,)),energies)[0]
+    
+    for img in range(nimages):
+        hist_ens = np.zeros((nenergies,nangles),dtype=int)
+        outname = '%s.%02i'%(outhead,img)
+        npulses = int(np.random.uniform(3,8))
+        ecenters = np.random.normal(ecentral,etotalwidth,(npulses,))
+        ewidths = np.random.gamma(5.,.125,(npulses,))+1
+        ephases= np.random.uniform(0.,2.*np.pi,(npulses,))
+
+        for i in range(npulses):
+            c0 = 1.
+            c2 = np.random.uniform(-1,1)
+            c4 = np.random.uniform(-(c0+c2),c0+c2)
+            poldist = np.polynomial.legendre.Legendre([c0,0,c2,0,c4])(np.cos(angles[:-1]))
+            for a in range(nangles):
+                ncounts = int(poldist[a] * scale)
+                if ncounts > 0:
+                    streak = streakamp*np.cos(angles[a]-ephases[i])
+                    hist_ens[:,a] += np.histogram(np.random.normal(ecenters[i]+streak,ewidths[i],(ncounts,)),energies)[0]
 
 
     np.savetxt(outname,hist_ens,fmt='%i')
